b5b2c5f492af3986c9646078b34e171f1a662db4
Rachida21 <79578355+Rachida21@users.noreply.github.com>
2021-02-24 17:37:41 +0100

    Add files via upload

---
+++ printArgs.java
@@ -0,0 +1,38 @@
+// EXO 4
+
+package myfirstproject1;
+
+import java.util.Scanner;
+
+public class printArgs;
+{
+	public static void main(String[] Args) 
+	{
+
+		int i;
+		
+		for (i = 0; i < Args.length; i++)
+		{
+
+			System.out.println(Args[i]);
+
+		 }
+
+		 
+		System.out.println(Args.length);
+
+		 
+	}
+
+		 
+}
+
+
+
+
+
+
+
+
+	
+	
