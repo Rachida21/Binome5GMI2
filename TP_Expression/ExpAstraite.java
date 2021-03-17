29ffd703d0ff446a6e1d25aae18f759031a53d71
Rachida21 <79578355+Rachida21@users.noreply.github.com>
2021-03-17 16:44:44 +0100

    Create ExpAstraite.java

---
+++ TP_Expression/ExpAstraite.java
@@ -0,0 +1,11 @@
+package Tp_Expressions;
+
+public abstract class ExpAbstraite {
+	
+	public abstract String toStringInfixe(); 
+	
+	public String toString;{
+		System.out.println("je suis une expression et me voila en notation infixée :" +this.toStringInfixe());
+	}
+}	
+	
